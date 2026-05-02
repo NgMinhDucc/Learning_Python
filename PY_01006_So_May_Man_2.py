6b42e420f8cbe97572cee60d2fc6d9ca843ba42b
NgMinhDucc <ducnm@gdscptit.dev>
2026-05-02 10:18:46 +0700

    added .gitignore

---
+++ PY_01006_So_May_Man_2.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    s = input()
+    flag = 1
+    for i in s:
+        if i != "4" or i != "7":
+            flag = 0
+            break
+    
+    print("YES" if flag == 1 else "NO")
